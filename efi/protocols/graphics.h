be10e48b926b8f9317aeedad3363f6b2bc0108d4
AJ <ajvallee15@gmail.com>
2026-04-15 23:14:13 +0100

    Updateded bootinfo, graphics.h and kernel makefile

---
+++ efi/protocols/graphics.h
@@ -0,0 +1,8 @@
+// efi/protocols/graphics.h
+// Graphics Output Protocol (GOP) from UEFI Specification 2.10, Section 12.9
+//
+// GOP replaces the older UGA (Universal Graphics Adapter) protocol.
+// It provides a simple way to get a framebuffer for graphics output.
+
+#pragma once
+
